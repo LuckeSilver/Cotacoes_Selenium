c879a2a0cbf77590fecd36ef4025ad108b85200d
luckesilver <jacks2kar@gmail.com>
2022-08-01 23:31:04 -0300

    ✨ Feat (app): Pegando cotação do Euro e do Ouro

--- src/app.py
+++ src/app.py
@@ -1,6 +1,8 @@
+from sys import displayhook
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from simple_chalk import chalk
+import pandas as pd
 
 #Abre o navegador
 navegador = webdriver.Chrome()
@@ -8,17 +10,28 @@ navegador = webdriver.Chrome()
 #Acessa o site que eu quiser
 navegador.get('https://www.google.com/')
 input_pesquisa_google = '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input'
-xpath_dolar = '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]'
+xpath_cotacao = '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]'
 
 #Pesquisando no google  e pegando cotação do dólar
 navegador.find_element('xpath', f'{input_pesquisa_google}').send_keys('Cotação dólar')
 navegador.find_element('xpath', f'{input_pesquisa_google}').send_keys(Keys.ENTER)
-cotacao_dolar = navegador.find_element('xpath', f'{xpath_dolar}').get_attribute('data-value')
+cotacao_dolar = navegador.find_element('xpath', f'{xpath_cotacao}').get_attribute('data-value')
 print(chalk.green(f'Cotação dolar hoje: R${cotacao_dolar}'))
+
+#Pesquisando no google  e pegando cotação do Euro
+navegador.get('https://www.google.com/')
+navegador.find_element('xpath', f'{input_pesquisa_google}').send_keys('Cotação Euro')
+navegador.find_element('xpath', f'{input_pesquisa_google}').send_keys(Keys.ENTER)
+cotacao_euro = navegador.find_element('xpath', f'{xpath_cotacao}').get_attribute('data-value')
+print(chalk.blue(f'Cotação dolar hoje: R${cotacao_euro}'))
 
 #Entrando no site melhor cambio e pegando cotação do ouro
 navegador.get('https://www.melhorcambio.com/ouro-hoje')
 cotacao_ouro = navegador.find_element('xpath', '//*[@id="comercial"]').get_attribute('value')
+cotacao_ouro = cotacao_ouro.replace(',', '.')
 print(chalk.yellow(f'Cotação ouro hoje: R${cotacao_ouro}'))
 print(chalk.green('Programa finalizado com Sucesso!'))
 navegador.close()
+
+# tabela = pd.read_excel('Produtos.xlsx')
+# print(tabela)
